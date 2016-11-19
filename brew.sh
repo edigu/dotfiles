61ae813dce112164460a737ce3b224d47290ab16
Yılmaz <yilmazsuslu@gmail.com>
2016-11-19 12:58:50 +0200

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Based on mathiasbynens/dotfiles
+
+# Install command-line tools using Homebrew.
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+brew tap homebrew/versions
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+# Install other useful binaries.
+brew install binutils
+brew install htop
+brew install ack
+brew install dark-mode
+brew install git-lfs
+brew install imagemagick --with-webp
+brew install lynx
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install speedtest_cli
+brew install ssh-copy-id
+brew install testssl
+brew install tree
+brew install vbindiff
+brew install webkit2png
+brew install youtube-dl
+brew install httpie
+brew install siege
+
+# Remove outdated versions from the cellar.
+brew cleanup
